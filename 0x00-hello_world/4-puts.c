4b37687046bae49c5c4e51309625a13f8163ebb4
tunjie <adetunjiakanjie@gmail.com>
2023-06-22 16:37:04 -0700

    puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
-*main - main entry point
-*Return : always 0(success)
+* main - A program that prints a line with the puts function
+* Return: 0 (Success)
 */
 int main(void)
 {
