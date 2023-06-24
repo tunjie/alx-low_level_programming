93508c097fd995b605641ba14cb34234664176e1
tunjie <adetunjiakanjie@gmail.com>
2023-06-24 10:30:07 -0700

    Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,8 +6,6 @@
   */
 int main(void)
 {
-char cont[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
-	puts(cont);
+fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
