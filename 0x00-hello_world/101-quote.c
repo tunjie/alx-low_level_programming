a05916c26fa1def2ec46c165556bc18f8663d256
tunjie <adetunjiakanjie@gmail.com>
2023-06-25 10:54:49 -0700

    Using fgets and puts function

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,6 +9,6 @@ int main(void)
 	char cont[83];
 
 	fgets(cont, 83, stderr);
-	puts("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	puts("and that piece of art is useful\"- Dora Korpar, 2015-10-19\n");
 	return (1);
 }
