a225dea207fc639ff6f5baff4c7cbec1b3820e74
tunjie <adetunjiakanjie@gmail.com>
2023-07-02 07:14:12 -0700

    Using 'write' function

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include <unistd.h>
 /**
   * main - A C program that prints a line to standard error
@@ -6,9 +5,6 @@
   */
 int main(void)
 {
-	char cont[83];
-
-	fgets(cont, 83, stderr);
-	puts("and that piece of art is useful\"- Dora Korpar, 2015-10-19\n");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
